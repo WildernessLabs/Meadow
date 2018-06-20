b270fb5a63bd0ca35ee2040ddc59434d26db3ab6
Geoff Norton <geoff@wildernesslabs.co>
2018-06-20 10:28:25 -0700

    [wip] We have 9 colbits, not 10

--- configs/stm32f777zit6-meadow/src/stm32_extmem.c
+++ configs/stm32f777zit6-meadow/src/stm32_extmem.c
@@ -198,7 +198,7 @@ void stm32_enablefmc(void)
            FMC_SDRAM_CR_BANKS_4 |
            FMC_SDRAM_CR_WIDTH_16 |
            FMC_SDRAM_CR_ROWBITS_13 |
-           FMC_SDRAM_CR_COLBITS_10,
+           FMC_SDRAM_CR_COLBITS_9,
       STM32_FMC_SDCR1);
 
   putreg32(FMC_SDRAM_CR_RPIPE_1 |
@@ -207,7 +207,7 @@ void stm32_enablefmc(void)
            FMC_SDRAM_CR_BANKS_4 |
            FMC_SDRAM_CR_WIDTH_16 |
            FMC_SDRAM_CR_ROWBITS_13 |
-           FMC_SDRAM_CR_COLBITS_10,
+           FMC_SDRAM_CR_COLBITS_9,
       STM32_FMC_SDCR2);
 
   putreg32((2 << FMC_SDRAM_TR_TRCD_SHIFT) |  /* tRCD min = 15ns */
