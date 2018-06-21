b2bf89dfbeec8a3fe901690484ff83f5dc3e706f
Geoff Norton <grompf@gmail.com>
2018-06-20 21:27:02 -0700

    [wip] Use the new stack extensions

--- mono/utils/mono-threads-nuttx.c
+++ mono/utils/mono-threads-nuttx.c
@@ -12,7 +12,14 @@
 void
 mono_threads_platform_get_stack_bounds (guint8 **staddr, size_t *stsize)
 {
-	g_assert_not_reached();
+	*staddr = (guint8*)pthread_get_stackaddr_np (pthread_self());
+	*stsize = pthread_get_stacksize_np (pthread_self());
+
+	printf ("staddr: %p\n", *staddr);
+	printf ("stsize: %d\n", *stsize);
+
+	/* staddr points to the start of the stack, not the end */
+	*staddr -= *stsize;
 }
 
 #endif
