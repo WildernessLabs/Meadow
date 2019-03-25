aeb4d1d7778dd9e79172db7a99eb888e782ff911
Joao Matos <joao@tritao.eu>
2019-03-25 12:19:04 +0000

    Added a script to install needed Homebrew packages.

---
+++ InstallPackages.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [[ $(command -v brew) == "" ]]; then
+    echo "Installing Homebrew"
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+    echo "Updating Homebrew"
+    brew update
+fi
+
+PACKAGES=(
+    osx-cross/arm/arm-gcc-bin
+    ccache
+    autoconf
+    libtool
+    cmake
+    libusb
+    automake
+    dfu-util
+)
+
+echo "Installing packages..."
+for pkg in ${PACKAGES[@]}
+do
+    if brew ls --versions $pkg > /dev/null; then
+        if brew outdated | grep -q $pkg; then
+            echo "Upgrading package $pkg..."
+            brew upgrade $pkg
+        else
+            echo "Package $pkg already installed."
+        fi
+    else
+        echo "Installing package $pkg..."
+        brew install $pkg
+    fi
+done
