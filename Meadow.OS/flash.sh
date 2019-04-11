a6a845df7d014a6ad21ae857396789173120447d
João Matos <joao@tritao.eu>
2019-04-11 10:46:16 +0100

    Improve the flashing scripts to be more robust and allow DFU and OpenOCD.

--- Meadow.OS/flash.sh
+++ Meadow.OS/flash.sh
@@ -19,6 +19,12 @@ case $i in
     -f|--force)
     FORCE=true
     ;;
+    -dfu|--dfu)
+    DFU=true
+    ;;
+    -ocd|--ocd|--openocd)
+    OCD=true
+    ;;
     *)
     # unknown option
     ;;
@@ -38,7 +44,11 @@ check_command_status() {
   if [ $exit_status -ne 0 ]; then
     printf " ${red}error${reset}\n"
     if ! $VERBOSE; then
-        printf "Re-run the script with --verbose flag to see the output.\n"
+      if [ "$DFU" = true ] ; then
+        printf "Try --ocd mode or --verbose flag to see the output.\n"
+      else
+        printf "Try --dfu mode or --verbose flag to see the output.\n"
+      fi
     fi
     exit 1
   else
@@ -50,11 +60,24 @@ check_command_status() {
 #   Flash the board with dfu-util
 #
 
-#dfu-util -a 0 -D $scriptdir/nuttx/nuttx.bin -s 0x08000000
-#dfu-util -a 0 -D $scriptdir/nuttx/nuttx_user.bin -s 0x08040000
+DFU_COMMON_FLAGS='--alt 0 --path 20-1'
+if [ "$DFU" = true ] ; then
+  printf "Flashing nuttx.bin using DFU... "
+  run_command "dfu-util $DFU_COMMON_FLAGS --download  $scriptdir/nuttx/nuttx.bin -s 0x08000000"
+  check_command_status
+  if [ -f $scriptdir/nuttx/nuttx_user.bin ]; then
+    printf "Flashing nuttx_user.bin using DFU... "
+    run_command "dfu-util $DFU_COMMON_FLAGS --download $scriptdir/nuttx/nuttx_user.bin -s 0x08040000"
+    check_command_status
+  fi
+  exit 0
+fi
 
 #
 #   Flash the board with openocd
 #
 
- openocd/src/openocd -s openocd/tcl -f interface/stlink-v2.cfg -f target/stm32f7x.cfg -f flash.cfg
+OCD=true
+printf "Flashing nuttx binaries using OpenOCD... "
+run_command "openocd/src/openocd -s openocd/tcl -f interface/stlink.cfg -f target/stm32f7x.cfg -f flash.cfg"
+check_command_status
