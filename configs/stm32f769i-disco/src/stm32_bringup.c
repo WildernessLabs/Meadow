1a2071a74d6d04c229a0909deafc37754c076a08
João Matos <joao@tritao.eu>
2019-06-28 00:01:35 +0100

    Enable PWM support for stm32f769i-disco board.

--- configs/stm32f769i-disco/src/stm32_bringup.c
+++ configs/stm32f769i-disco/src/stm32_bringup.c
@@ -1,7 +1,7 @@
 /****************************************************************************
- * config/stm32f769i-disco/src/stm32_appinitialize.c
+ * config/stm32f769i-disco/src/stm32_bringup.c
  *
- *   Copyright (C) 2015-2016, 2019 Gregory Nutt. All rights reserved.
+ *   Copyright (C) 2012, 2014-2018 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>
  *
  * Redistribution and use in source and binary forms, with or without
@@ -38,9 +38,13 @@
  ****************************************************************************/
 
 #include <nuttx/config.h>
-#include <sys/types.h>
 
-#include "stm32_ccm.h"
+#include <sys/mount.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <debug.h>
+#include <errno.h>
+
 #include "stm32f769i-disco.h"
 
 /****************************************************************************
@@ -48,62 +52,32 @@
  ****************************************************************************/
 
 /****************************************************************************
- * Name: board_app_initialize
+ * Name: stm32_bringup
  *
  * Description:
- *   Perform application specific initialization.  This function is never
- *   called directly from application code, but only indirectly via the
- *   (non-standard) boardctl() interface using the command BOARDIOC_INIT.
+ *   Perform architecture-specific initialization
  *
- * Input Parameters:
- *   arg - The boardctl() argument is passed to the board_app_initialize()
- *         implementation without modification.  The argument has no
- *         meaning to NuttX; the meaning of the argument is a contract
- *         between the board-specific initalization logic and the
- *         matching application logic.  The value cold be such things as a
- *         mode enumeration value, a set of DIP switch switch settings, a
- *         pointer to configuration data read from a file or serial FLASH,
- *         or whatever you would like to do with it.  Every implementation
- *         should accept zero/NULL as a default configuration.
+ *   CONFIG_BOARD_LATE_INITIALIZE=y :
+ *     Called from board_late_initialize().
  *
- * Returned Value:
- *   Zero (OK) is returned on success; a negated errno value is returned on
- *   any failure to indicate the nature of the failure.
+ *   CONFIG_BOARD_LATE_INITIALIZE=n && CONFIG_LIB_BOARDCTL=y :
+ *     Called from the NSH library
  *
  ****************************************************************************/
 
-int board_app_initialize(uintptr_t arg)
+int stm32_bringup(void)
 {
-#ifdef CONFIG_FS_PROCFS
-  int ret;
+    int ret;
 
-#ifdef CONFIG_STM32_CCM_PROCFS
-  /* Register the CCM procfs entry.  This must be done before the procfs is
-   * mounted.
-   */
+#ifdef CONFIG_PWM
+  /* Initialize PWM and register the PWM device. */
 
-  (void)ccm_procfs_register();
-#endif
-
-  /* Mount the procfs file system */
-
-  ret = mount(NULL, SAMV71_PROCFS_MOUNTPOINT, "procfs", 0, NULL);
+  ret = stm32_pwm_setup();
   if (ret < 0)
     {
-      SYSLOG("ERROR: Failed to mount procfs at %s: %d\n",
-             SAMV71_PROCFS_MOUNTPOINT, ret);
+      syslog(LOG_ERR, "ERROR: stm32_pwm_setup() failed: %d\n", ret);
     }
 #endif
 
-#ifdef CONFIG_BOARD_LATE_INITIALIZE
-  /* Board initialization already performed by board_late_initialize() */
-
-  return OK;
-#else
-  /* Perform board-specific initialization */
-
-  return stm32_bringup();
-#endif
-
-  return OK;
+  return ret;
 }
