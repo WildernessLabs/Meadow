7719962723d3271c4034b185bbdd5f67e8edd8b4
Peter-Moody <peter.moody@wildernesslabs.co>
2019-08-05 10:35:03 -0700

    file renamed

--- configs/stm32f777zit6-meadow/src/hcom/hcom_usb_acm_interface.c
+++ configs/stm32f777zit6-meadow/src/hcom/hcom_usb_acm_interface.c
@@ -1,5 +1,5 @@
 /****************************************************************************
- * configs/stm32f777-zit6-meadow/src/hcom_host_com_xmit_rcv.c
+ * configs/stm32f777-zit6-meadow/src/hcom_usb_acm_interface.c
  * 
  *   Copyright (C) 2019 Wilderness Labs. All rights reserved.
  *   Copyright (C) 2017 Gregory Nutt. All rights reserved.
