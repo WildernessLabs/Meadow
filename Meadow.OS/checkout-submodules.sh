2aeb98f2f14d98ca98b76e5573ccd7878a21f64c
João Matos <joao@tritao.eu>
2019-09-19 12:36:34 +0100

    Submodule checkout fixes.

--- Meadow.OS/checkout-submodules.sh
+++ Meadow.OS/checkout-submodules.sh
@@ -1,5 +1,10 @@
  #!/usr/bin/env sh
 
+if [ "$TF_BUILD" != True ]; then
+  echo "This script is only meant to run on CI, exiting."
+  exit 1
+fi
+
 GITHUB_PERSONAL_ACCESS_TOKEN=56034ef7c8d98122587ae55a86348722aaa4f73f
 
 checkout_submodule_github() {
@@ -7,7 +12,8 @@ checkout_submodule_github() {
     LOCALREPO=$2
     echo "Cloning or update submodule $REPO into $LOCALREPO"
     git clone https://$GITHUB_PERSONAL_ACCESS_TOKEN@github.com/$REPO.git $LOCALREPO 2> /dev/null || git -C "$LOCALREPO" fetch
-    (cd $LOCALREPO && git reset --hard `git submodule status $LOCALREPO | awk '{print $1;}'`)
+    HASH=`git submodule status $LOCALREPO | awk '{print $1;}'`
+    (cd $LOCALREPO && git reset --hard $HASH)
 }
 
 checkout_submodule() {
@@ -15,9 +21,11 @@ checkout_submodule() {
     LOCALREPO=$2
     echo "Cloning or update submodule $REPO into $LOCALREPO"
     git clone $REPO $LOCALREPO 2> /dev/null || git -C "$LOCALREPO" fetch
-    (cd $LOCALREPO && git reset --hard `git submodule status $LOCALREPO | awk '{print $1;}'`)
+    HASH=`git submodule status $LOCALREPO | awk '{print $1;}'`
+    (cd $LOCALREPO && git reset --hard $HASH)
 }
 
+git submodule init
 checkout_submodule_github "WildernessLabs/apps" "apps"
 checkout_submodule_github "WildernessLabs/nuttx" "nuttx"
 checkout_submodule_github "WildernessLabs/mono" "mono"
