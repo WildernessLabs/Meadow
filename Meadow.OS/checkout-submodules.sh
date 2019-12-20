cb8acf6eab2c019440f492c3cea0674322a5a7d0
Joao Matos <joao@tritao.eu>
2019-12-20 18:55:47 +0000

    Improve CI submodule checkout script for Mono submodules.

--- Meadow.OS/checkout-submodules.sh
+++ Meadow.OS/checkout-submodules.sh
@@ -1,4 +1,6 @@
- #!/usr/bin/env sh
+#!/bin/bash
+
+scriptdir="$( cd "$(dirname "$0")" ; pwd -P )"
 
 if [ "$TF_BUILD" != True ]; then
   echo "This script is only meant to run on CI, exiting."
@@ -7,11 +9,17 @@ fi
 
 GITHUB_PERSONAL_ACCESS_TOKEN=56034ef7c8d98122587ae55a86348722aaa4f73f
 
-checkout_submodule_github() {
+clone_or_fetch_submodule_github() {
     REPO=$1
     LOCALREPO=$2
     echo "Cloning or update submodule $REPO into $LOCALREPO"
     git clone https://$GITHUB_PERSONAL_ACCESS_TOKEN@github.com/$REPO.git $LOCALREPO 2> /dev/null || git -C "$LOCALREPO" fetch
+}
+
+checkout_submodule_github() {
+    REPO=$1
+    LOCALREPO=$2
+    clone_or_fetch_submodule_github $REPO $LOCALREPO
     HASH=`git submodule status $LOCALREPO | awk '{print $1;}'`
     (cd $LOCALREPO && git reset --hard $HASH)
 }
@@ -28,6 +36,7 @@ checkout_submodule() {
 git submodule init
 checkout_submodule_github "WildernessLabs/apps" "apps"
 checkout_submodule_github "WildernessLabs/nuttx" "nuttx"
-checkout_submodule_github "WildernessLabs/mono" "mono"
-checkout_submodule_github "WildernessLabs/nuttx" "nuttx"
 checkout_submodule "https://bitbucket.org/nuttx/tools.git" "tools"
+
+checkout_submodule_github "WildernessLabs/mono" "mono"
+clone_or_fetch_submodule_github "WildernessLabs/corefx" "mono/external/corefx"
