603ffe2f88bf7b36bd4e5c977d40ef28a06c4b62
Joao Matos <joao@tritao.eu>
2020-02-17 18:23:18 +0000

    Clean up the HCOM sub-folders object files properly.

--- Meadow.OS/build.sh
+++ Meadow.OS/build.sh
@@ -125,7 +125,7 @@ NUTTX_CONFIG="stm32f777zit6-meadow/$CONFIG"
 if [ -r "$scriptdir/nuttx/.config" ] && ($FORCE || $CLEAN); then
     printf "Cleaning NuttX (already configured)..."
     run_command "make -C $scriptdir/nuttx distclean -j8"
-    run_command "rm -f $scriptdir/nuttx/configs/stm32f777zit6-meadow/src/hcom/**.o"
+    run_command "rm -f $scriptdir/nuttx/configs/stm32f777zit6-meadow/src/hcom/**/*.o"
     run_command "rm -f $scriptdir/nuttx/Meadow.OS.bin"
     check_command_status
 fi
