2039a2733ef6323fb54a85a2aebbc36061b97f04
Joao Matos <joao@tritao.eu>
2020-02-26 23:24:01 +0000

    Implement mono_native_thread_os_id_get() for NuttX threads.

--- mono/utils/mono-threads-nuttx.c
+++ mono/utils/mono-threads-nuttx.c
@@ -8,6 +8,7 @@
 
 #include <mono/utils/mono-threads.h>
 #include <pthread.h>
+#include <unistd.h>
 
 void
 mono_threads_platform_get_stack_bounds (guint8 **staddr, size_t *stsize)
@@ -17,6 +18,15 @@ mono_threads_platform_get_stack_bounds (guint8 **staddr, size_t *stsize)
 
 	/* staddr points to the start of the stack, not the end */
 	*staddr -= *stsize;
+}
+
+guint64
+mono_native_thread_os_id_get (void)
+{
+	// thrd_current() would probably be better semantically here,
+	// but NuttX threads.h fails to be included. Its just a simple define
+	// wrapper over getpid() anyway so just call it directly.
+	return (guint64)getpid();
 }
 
 #endif
