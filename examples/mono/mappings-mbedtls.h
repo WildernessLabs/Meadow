d0d4bff3722e26853e89138384ac817a58765eb7
Alexis Christoforides <alexis@thenull.net>
2021-02-03 19:12:46 -0500

    Update mappings-mbedtls.h

--- examples/mono/mappings-mbedtls.h
+++ examples/mono/mappings-mbedtls.h
@@ -1,5 +1,4 @@
 extern intptr_t mono_mbedtls_init (intptr_t mono_fd, intptr_t readbuf, intptr_t writebuf);
-intptr_t mono_mbedtls_init (intptr_t mono_fd, intptr_t readbuf, intptr_t writebuf);
 extern int mono_mbedtls_read (intptr_t * ctx, int length);
 extern int mono_mbedtls_write (intptr_t * ctx, int length);
 extern void mono_mbedtls_close (intptr_t * ctx);
