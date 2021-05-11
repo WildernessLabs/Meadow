563cc28d15e5266d400f902d8c4f0536f897786d
Mark Stevens <mark@thepcsite.co.uk>
2021-05-11 17:19:41 +0100

    Added clean of YAML library object files.

--- Meadow.OS/build.sh
+++ Meadow.OS/build.sh
@@ -167,6 +167,8 @@ if [ -r "$scriptdir/nuttx/.config" ] && ($FORCE || $CLEAN); then
     run_command "rm -f $scriptdir/nuttx/configs/stm32f777zit6-meadow/src/hcom_nx/**/*.o"
     run_command "rm -f $scriptdir/nuttx/configs/stm32f777zit6-meadow/src/hcom_nx/*.o"
     run_command "rm -f $scriptdir/nuttx/configs/stm32f777zit6-meadow/src/espcp/*.o"
+    run_command "rm -f $scriptdir/nuttx/configs/stm32f777zit6-meadow/src/libcyaml/*.o"
+    run_command "rm -f $scriptdir/nuttx/configs/stm32f777zit6-meadow/src/libcyaml/*.o"
     run_command "rm -f $scriptdir/apps/examples/hcom/**/*.o"
     run_command "rm -f $scriptdir/apps/examples/hcom/*.o"
     run_command "rm -f $scriptdir/nuttx/Meadow.OS.bin"
