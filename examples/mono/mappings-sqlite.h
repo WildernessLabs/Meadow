d6e25c61e2ea6768236b04688a0b2c06689db95d
Chris Tacke <christopher.tacke@hypergiant.com>
2021-07-11 10:35:36 -0500

    re-attempt at sqlite

---
+++ examples/mono/mappings-sqlite.h
@@ -0,0 +1,15 @@
+﻿#ifndef __INC_SQLITE_FUNCTIONS__
+#define __INC_SQLITE_FUNCTIONS__
+
+#include "../sqlite/sqlite3.h"
+
+extern int sqlite3_open(const char *, struct sqlite3 **);
+extern int sqlite3_os_init(void);
+
+MonoDlMapping sqlite_mappings[] = {
+	{ "sqlite3_open", sqlite3_open },
+	{ "sqlite3_os_init", sqlite3_os_init },
+    { NULL, NULL }
+};
+
+#endif // __INC_SQLITE_FUNCTIONS__
