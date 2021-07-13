2cee2c5aa5778131867af786b4637df31fad65b5
Mark Stevens <mark@thepcsite.co.uk>
2021-07-13 18:12:52 +0100

    Added ESP version information to syslog

--- configs/stm32f777zit6-meadow/src/espcp/espcp_event_handlers.c
+++ configs/stm32f777zit6-meadow/src/espcp/espcp_event_handlers.c
@@ -194,6 +194,7 @@ void espcp_system_get_configuration_event_handler(espcp_message_t *message)
                     if (meadow_configuration->esp_software_version == NULL)
                     {
                         meadow_configuration->esp_software_version = strdup(config->esp_config->software_version);
+                        syslog(LOG_INFO, "ESP32 Coprocessor ready, firmware version %s\n", meadow_configuration->esp_software_version);
                     }
                 }
                 else
