ca12a157d762980c6c7d20bafeb2e0a565d8ce3e
Nikolas Ioannides <nikolas.ioannides@wildernesslabs.co>
2021-09-07 16:13:49 +0300

    Cleanup and Refactor

--- bootloader/Core/Inc/bootloader.h
+++ bootloader/Core/Inc/bootloader.h
@@ -51,12 +51,8 @@ enum bootloader_state{
 	bootloader_backup = 3,
 };
 
-/* Exported functions prototypes ---------------------------------------------*/
 void Error_Handler(void);
-
-/* USER CODE BEGIN EFP */
 uint8_t getOTAFlagState(uint8_t flag);
-/* USER CODE END EFP */
 
 #ifdef __cplusplus
 }
