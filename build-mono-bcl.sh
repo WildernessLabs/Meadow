567dfd6706e9aa7f8ee8e0d2e324ba426814fee1
Alexis Christoforides <alexis@thenull.net>
2021-11-04 11:33:43 -0400

    Add Microsoft.Bcl.AsyncInterfaces facade to BCL package (#95) (#97)
    Needed for some netstandard2.1 nugets.

--- build-mono-bcl.sh
+++ build-mono-bcl.sh
@@ -125,6 +125,7 @@ function buildMonoBCL {
   run_command "make -C $MONO_DIR -j8 PROFILE_PLATFORM=linux HOST_PLATFORM=linux"
   run_command "make -C ${MONO_DIR}/mcs/class/Facades/System.Memory PROFILE_PLATFORM=linux HOST_PLATFORM=linux"
   run_command "make -C ${MONO_DIR}/mcs/class/Facades/System.Buffers PROFILE_PLATFORM=linux HOST_PLATFORM=linux"
+  run_command "make -C ${MONO_DIR}/mcs/class/Facades/Microsoft.Bcl.AsyncInterfaces PROFILE_PLATFORM=linux HOST_PLATFORM=linux"
   check_command_status
 }
 
