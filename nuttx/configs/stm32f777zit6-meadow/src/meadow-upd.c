aa692c45ba4c1e2a477709cacbd94019afd94fd1
Chris Tacke <christopher.tacke@hypergiant.com>
2022-03-17 21:18:34 -0500

    Added comms busses

--- nuttx/configs/stm32f777zit6-meadow/src/meadow-upd.c
+++ nuttx/configs/stm32f777zit6-meadow/src/meadow-upd.c
@@ -379,8 +379,6 @@ static int upd_handle_i2c(int cmd, struct upd_i2c_cmd* data)
   struct i2c_config_s *pCfg;
   struct i2c_master_s *pBus;
 
-    printf("meadow_upd I2C bus #%d\n", data->busNumber);
-
   if(data->busNumber == 0 || data->busNumber == 1)
   {
     if(g_i2c1 == NULL)
@@ -395,7 +393,6 @@ static int upd_handle_i2c(int cmd, struct upd_i2c_cmd* data)
     if(g_i2c3 == NULL)
     {
       g_i2c3 = stm32_i2cbus_initialize(MEADOW_I2C_PORT3);
-    printf("meadow_upd I2C init #%d\n", g_i2c3);
     }
     pBus = g_i2c3;
     pCfg = &g_i2c3_cfg;
@@ -417,7 +414,6 @@ static int upd_handle_i2c(int cmd, struct upd_i2c_cmd* data)
     if(data->rxLength > 0)
     {
       // writeread
-      printf("meadow_upd I2C writeread\n");
       result = i2c_writeread(pBus, pCfg, data->txBuffer, data->txLength, data->rxBuffer, data->rxLength);
     }
     else
