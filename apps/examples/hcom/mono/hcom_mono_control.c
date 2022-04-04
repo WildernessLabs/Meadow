40c7cb9bb751313f55000fad526c451664754a4e
Mark Stevens <mark.stevens@wildernesslabs.co>
2022-04-04 17:23:32 +0100

    Added comments and changed behaviour for --jit and --aot

--- apps/examples/hcom/mono/hcom_mono_control.c
+++ apps/examples/hcom/mono/hcom_mono_control.c
@@ -148,7 +148,7 @@ int hcom_mono_ctrl_mono_main_setup()
 }
 
 //
-//  TODO: Add stricmp and strnicmp to the stanard library.
+//  TODO: Add stricmp and strnicmp to the standard library.
 //
 static int stricmp(FAR const char *cs, FAR const char *ct)
 {
@@ -176,6 +176,25 @@ int strnicmp(const char *cs, const char *ct, size_t nb)
   return result;
 }
 
+/****************************************************************************
+ * Name: hcom_mono_ctrl_add_command_line_option
+ *
+ * Description:
+ *  Add a command line option to the array of options expanding the array in
+ *  the process.
+ *
+ * Input Parameters:
+ *  current_options - Current list of known options
+ *  option - Option to be added.
+ *  option_count - Pointer to the current option count.
+ *
+ * Returned Value:
+ *  Pointer to the array of options or NULL if the allocation failed.
+ *
+ * Assumptions/Limitations:
+ *  None.
+ *
+ ****************************************************************************/
 char **hcom_mono_ctrl_add_command_line_option(char **current_options, char *option, int *option_count)
 {
   char **result = (char **) realloc(current_options, (*option_count + 2) * sizeof(char *));
@@ -196,10 +215,11 @@ char **hcom_mono_ctrl_add_command_line_option(char **current_options, char *opti
  *  so that they can be used by Mono.
  *
  * Input Parameters:
- *  options - string of options.
+ *  options - String of options from the configuration file.
+ *  count - Pointer to the count of options found.
  *
  * Returned Value:
- *  Number of options found.
+ *  Pointer the an array of string that are the options to be passed to Mono.
  *
  * Assumptions/Limitations:
  *  List of valid option prefixes is configured (see the head of this file).
