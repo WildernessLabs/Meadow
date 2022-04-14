cbe357c1011a1b8875c4010889727f4fa53efa27
Mark Stevens <mark.stevens@wildernesslabs.co>
2022-04-14 13:19:05 +0100

    Preliminary change to BCL build checked in for comment.

--- build-mono-bcl.sh
+++ build-mono-bcl.sh
@@ -33,7 +33,9 @@ VERBOSE=false
 FORCE=false
 CLEAN=false
 DEBUG=false
-MONO_DIR=$scriptdir/monobcl
+MONO_BCL_DIR=$scriptdir/monobcl
+MONO_DIR=$scriptdir/mono
+KEEP_PDBS=false
 NETCORE=false
 
 for i in "$@"
@@ -50,6 +52,9 @@ case $i in
   ;;
   -d|--debug)
   DEBUG=true
+  ;;
+  -k|--keeppdbs)
+  KEEP_PDBS=true
   ;;
   --netcore)
   NETCORE=true
@@ -82,44 +87,47 @@ check_command_status() {
   fi
 }
 
-function copyMonoToBCL {
-  if $VERBOSE; then
-    RSYNC_FLAGS="-v --progress"
-  else
-    RSYNC_FLAGS=
-  fi
-  if [[ "$OS" == "mac" ]]; then
-    cd $scriptdir/mono
-    rsync -ar $RSYNC_FLAGS --delete . $MONO_DIR
-    cd ..
-  else
-    rsync -a $RSYNC_FLAGS --delete mono/ $MONO_DIR
-  fi
-}
-
-function cleanBCLDirectory {
-  # if [ ! -d $MONO_DIR ]; then
-  # fi
-  cd $MONO_DIR
-
+function updateBCLDirectory {
   if $CLEAN; then
-    printf "Cleaning the Mono build tree...\n"
-    git clean -xfd .
-    git submodule foreach --recursive git clean -xfd
+    printf "Cleaning BCL..."
+    if [ -d $MONO_BCL_DIR ]; then
+      rm -R $MONO_BCL_DIR
+    fi
+    git clean -xffd mono/
+    git submodule update --init --recursive
+    printf " - done\n"
   fi
 
+  if [ -d $MONO_BCL_DIR ]; then
+    printf "Synchronising $MONO_DIR with $MONO_BCL_DIR"
+    if $VERBOSE; then
+      RSYNC_FLAGS="-v --progress"
+    else
+      RSYNC_FLAGS=
+    fi
+    if [[ "$OS" == "mac" ]]; then
+      pushd . &>/dev/null
+      cd $MONO_DIR
+      rsync -ar $RSYNC_FLAGS . $MONO_BCL_DIR
+      #
+      # Delete option here pushes incremental builds from 40s to 15m.
+      #
+      # rsync -ar $RSYNC_FLAGS --delete . $MONO_BCL_DIR
+      popd &>/dev/null
+    else
+      rsync -a $RSYNC_FLAGS --delete mono/ $MONO_BCL_DIR
+    fi
+  else
+    printf "Copying $MONO_DIR to $MONO_BCL_DIR"
+    cp -R $MONO_DIR $MONO_BCL_DIR
+  fi
+  printf " - done\n"
 }
 
 function configureMonoBCL {
-  cd $MONO_DIR
+  cd $MONO_BCL_DIR
 
-  if $CLEAN; then
-    printf "Cleaning the Mono build tree...\n"
-    git clean -xfd .
-    git submodule foreach --recursive git clean -xfd
-  fi
-
-  if [ ! -f $MONO_DIR/configure ] || $FORCE || $CLEAN; then
+  if [ ! -f $MONO_BCL_DIR/configure ] || $FORCE || $CLEAN; then
     printf "Running autogen.sh...\n"
     NOCONFIGURE=1 ./autogen.sh
   fi
@@ -132,7 +140,7 @@ function configureMonoBCL {
       --enable-mbedtls
       --disable-nls"
 
-  if [ ! -f $MONO_DIR/Makefile ] || $FORCE || $CLEAN; then
+  if [ ! -f $MONO_BCL_DIR/Makefile ] || $FORCE || $CLEAN; then
       printf "Configuring Mono BCL...\n"
 
       # This step does not use run_command because of bash string escaping issues.
@@ -151,24 +159,26 @@ function buildMonoBCL {
   printf "Building Mono BCL...\n"
   OLD_VERBOSE=$VERBOSE
   VERBOSE=true
-  run_command "make -C $MONO_DIR -j8"
-  run_command "make -C $MONO_DIR -j8 PROFILE_PLATFORM=linux HOST_PLATFORM=linux"
-  run_command "make -C ${MONO_DIR}/mcs/class/Facades/System.Memory PROFILE_PLATFORM=linux HOST_PLATFORM=linux"
-  run_command "make -C ${MONO_DIR}/mcs/class/Facades/System.Buffers PROFILE_PLATFORM=linux HOST_PLATFORM=linux"
-  run_command "make -C ${MONO_DIR}/mcs/class/Facades/Microsoft.Bcl.AsyncInterfaces PROFILE_PLATFORM=linux HOST_PLATFORM=linux"
-  run_command "make -C ${MONO_DIR}/mcs/class/Facades/System.Threading.Tasks.Extensions PROFILE_PLATFORM=linux HOST_PLATFORM=linux"
+  run_command "make -C $MONO_BCL_DIR -j8"
+  run_command "make -C $MONO_BCL_DIR -j8 PROFILE_PLATFORM=linux HOST_PLATFORM=linux"
+  run_command "make -C ${MONO_BCL_DIR}/mcs/class/Facades/System.Memory PROFILE_PLATFORM=linux HOST_PLATFORM=linux"
+  run_command "make -C ${MONO_BCL_DIR}/mcs/class/Facades/System.Buffers PROFILE_PLATFORM=linux HOST_PLATFORM=linux"
+  run_command "make -C ${MONO_BCL_DIR}/mcs/class/Facades/Microsoft.Bcl.AsyncInterfaces PROFILE_PLATFORM=linux HOST_PLATFORM=linux"
+  run_command "make -C ${MONO_BCL_DIR}/mcs/class/Facades/System.Threading.Tasks.Extensions PROFILE_PLATFORM=linux HOST_PLATFORM=linux"
   VERBOSE=$OLD_VERBOSE
   check_command_status
 }
 
 function packageMonoBCL {
   printf "Packaging Mono...\n"
-  mkdir -p $MONO_DIR/libs/bcl
-  rm -rf $MONO_DIR/libs/bcl
-  cp -R $MONO_DIR/mcs/class/lib/net_4_x-linux $MONO_DIR/libs/bcl
-  pushd $MONO_DIR/libs/bcl
-  cat <$scriptdir/bcl-pdb-blacklist.txt | xargs -n 10 rm
+  mkdir -p $MONO_BCL_DIR/libs/bcl
+  rm -rf $MONO_BCL_DIR/libs/bcl
+  cp -R $MONO_BCL_DIR/mcs/class/lib/net_4_x-linux $MONO_BCL_DIR/libs/bcl
+  pushd $MONO_BCL_DIR/libs/bcl
   cat <$scriptdir/bcl-blacklist.txt | xargs -n 10 rm
+  if ! $KEEP_PDBS; then
+    cat <$scriptdir/bcl-pdb-blacklist.txt | xargs -n 10 rm
+  fi
   popd
   check_command_status
 }
@@ -185,40 +195,25 @@ MONO_CORLIB_VERSION = 423e7794-9279-49a3-a477-f1cb2432e9f4
 HOST_PLATFORM ?= linux
 END
 )
-  echo "$CONFIG_MAKE" > $MONO_DIR/netcore/config.make
+  echo "$CONFIG_MAKE" > $MONO_BCL_DIR/netcore/config.make
 }
 
 function buildNetCoreBCL {
   printf "Building Mono .NET Core BCL...\n"
-  COREARCH=arm make -C $MONO_DIR/netcore bcl
+  COREARCH=arm make -C $MONO_BCL_DIR/netcore bcl
 }
 
 function packageNetCoreBCL {
   printf "Packaging Mono .NET Core BCL...\n"
-  rm -rf $MONO_DIR/libs/bcl
-  mkdir -p $MONO_DIR/libs/bcl
-  cp $MONO_DIR/netcore/System.Private.CoreLib/bin/arm/*System.Private.CoreLib.{dll,pdb,xml} $MONO_DIR/libs/bcl
+  rm -rf $MONO_BCL_DIR/libs/bcl
+  mkdir -p $MONO_BCL_DIR/libs/bcl
+  cp $MONO_BCL_DIR/netcore/System.Private.CoreLib/bin/arm/*System.Private.CoreLib.{dll,pdb,xml} $MONO_BCL_DIR/libs/bcl
 }
 
-if [ ! -d $MONO_DIR ]; then
-  git clean -xffd $MONO_DIR/
-  git submodule update --init --recursive
-  if $VERBOSE; then
-    RSYNC_FLAGS="-v --progress"
-  else
-    RSYNC_FLAGS=
-  fi
-  if [[ "$OS" == "mac" ]]; then
-    cd $scriptdir/mono
-    rsync -ar $RSYNC_FLAGS --delete . $MONO_DIR
-    cd ..
-  else
-    rsync -a $RSYNC_FLAGS --delete mono/ $MONO_DIR
-  fi
-fi
+updateBCLDirectory
 
-mkdir -p $MONO_DIR/bcl
-cd $MONO_DIR/bcl
+mkdir -p $MONO_BCL_DIR/bcl
+cd $MONO_BCL_DIR/bcl
 
 #
 # Configure, build and package Mono / .NET Core BCL
