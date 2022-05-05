aef916ecac4703cd4bfee1501781d6674530ba29
Mark Stevens <mark.stevens@wildernesslabs.co>
2022-05-05 15:30:01 +0100

    Added getifaddrs etc from latest release.

--- nuttx/include/net/if.h
+++ nuttx/include/net/if.h
@@ -147,6 +147,7 @@ struct mii_ioctl_data_s
 struct lifreq
 {
   char                        lifr_name[IFNAMSIZ];      /* Network device name (e.g. "eth0") */
+  int16_t                     lifr_ifindex;             /* Interface index */
   union
   {
     struct sockaddr_storage   lifru_addr;               /* IP Address */
