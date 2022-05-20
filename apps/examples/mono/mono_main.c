9ab3aa2879ad31828ff913d7d0ee8cf9b63233e6
Alexis Christoforides <alexis@thenull.net>
2022-05-20 02:03:58 -0400

    [mono] Remove accidental 10 second sleep on startup
    It was added accidentally by https://github.com/WildernessLabs/Meadow/commit/da7ded3694bfc6b2b4b783608d56ffd1fbc565e5

--- apps/examples/mono/mono_main.c
+++ apps/examples/mono/mono_main.c
@@ -160,7 +160,6 @@ int mono_main(int hcom_argc, char *hcom_argv[])
   setenv("MONO_LOG_LEVEL", "debug", 1);
   setenv("MONO_GC_PARAMS", "max-heap-size=8m,nursery-size=512k,soft-heap-limit=4m,major=marksweep", 1);
 
-  sleep(10);
 #ifdef CONFIG_MTD_PARTITION
   mono_set_assemblies_path("/meadow0");
 #else
