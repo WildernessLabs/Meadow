a07592184c7a1f58c3567a47340e6bdc327f72e3
Neale Ferguson <neale@sinenomine.net>
2022-09-08 17:24:35 +1000

    * Reinstate the isfinite macros

--- nuttx/include/nuttx/lib/math.h
+++ nuttx/include/nuttx/lib/math.h
@@ -110,8 +110,12 @@
 #define isinfl(x)  (((x) == INFINITY_L) || ((x) == -INFINITY_L))
 
 #define finite(x)  (!(isinf(x) || isnan(x)))
-#define finitef(x) (!(isinf_f(x) || isnan(x)))
-#define finitel(x) (!(isinf_l(x) || isnan(x)))
+#define finitef(x) (!(isinff(x) || isnanf(x)))
+#define finitel(x) (!(isinfl(x) || isnanl(x)))
+
+#define isfinite(x)  (!(isinf(x) || isnan(x)))
+#define isfinitef(x) (!(isinff(x) || isnanf(x)))
+#define isfinitel(x) (!(isinfl(x) || isnanl(x)))
 
 /* Exponential and Logarithmic constants ************************************/
 
