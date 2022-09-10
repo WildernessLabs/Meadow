86e3b582c2143c4b2c746b00a57964c54df0372a
Mark Stevens <mark.stevens@wildernesslabs.co>
2022-09-10 20:17:53 +0100

    Added the ability to remove freed blocks.

--- gdb/Tracing.py
+++ gdb/Tracing.py
@@ -57,12 +57,34 @@ class AddHeapTrace(gdb.Command):
         global global_tracing
         if global_tracing:
             bt = NuttxAndMonoBacktrace()
-            address = long(gdb.parse_and_eval("ret"))
-            global_heap_information[address] = bt.backtrace(include_managed_code=False)
+            address = long(gdb.parse_and_eval('ret'))
+            heapdata = {}
+            heapdata['requested'] = long(gdb.parse_and_eval('size'))
+            heapdata['allocated'] = long(gdb.parse_and_eval('alignsize'))
+            heapdata['backtrace'] = bt.backtrace(include_managed_code=False)
+            global_heap_information[address] = heapdata
         else:
             print('Cannot collect heap information as tracing is disabled')
 
 AddHeapTrace()
+
+class RemoveHeapTrace(gdb.Command):
+    def __init__(self):
+        super(RemoveHeapTrace, self).__init__("trace_remove_heaptrace", gdb.COMMAND_STACK)
+
+    def invoke(self, arg, from_tty):
+        global global_heap_information
+        global global_tracing
+        if global_tracing:
+            address = long(gdb.parse_and_eval("mem"))
+            if address in global_heap_information.keys():
+                print('Freeing address 0x%0.8x' % address)
+                del global_heap_information[address]
+            else:
+                raise Exception('Cannot find memory allocation for address 0x%0.8x' % address)
+
+
+RemoveHeapTrace()
 
 class ShowTraceData(gdb.Command):
     def __init__(self):
@@ -87,8 +109,9 @@ class ShowHeapTraceData(gdb.Command):
     def invoke(self, arg, from_tty):
         global global_heap_information
         for heapdata in global_heap_information:
-            print('Memory allocation 0x%0.8x' % heapdata)
-            for line in global_heap_information[heapdata]:
+            heapinfo = global_heap_information[heapdata]
+            print('Memory allocation 0x%0.8x, requested %d, allocated %d' %(heapdata, heapinfo['requested'], heapinfo['allocated']))
+            for line in heapinfo['backtrace']:
                 print('    %s' %line)
 
 ShowHeapTraceData()
