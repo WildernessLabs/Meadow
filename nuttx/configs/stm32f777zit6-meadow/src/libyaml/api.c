f7ab4b7b92408cee6cce8b53ea5245f6f65b972a
Mark Stevens <mark.stevens@wildernesslabs.co>
2022-09-21 05:34:36 +0100

    Speeeeling

--- nuttx/configs/stm32f777zit6-meadow/src/libyaml/api.c
+++ nuttx/configs/stm32f777zit6-meadow/src/libyaml/api.c
@@ -88,7 +88,7 @@ yaml_strdup(const yaml_char_t *str)
         memcpy(result, str, len + 1);
     }
 
-    MEADOW_TRACE_INFORMATION("Duplicating string, new string al location %p\n", result);
+    MEADOW_TRACE_INFORMATION("Duplicating string, new string at location %p\n", result);
     return (yaml_char_t *) result;
 }
 
