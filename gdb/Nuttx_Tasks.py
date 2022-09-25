c15942e8ded4cc81f0e9a16f6057ba6b5198294d
Mark Stevens <mark.stevens@wildernesslabs.co>
2022-09-25 19:46:04 +0100

    Now prints both user and kernel heap.

--- gdb/Nuttx_Tasks.py
+++ gdb/Nuttx_Tasks.py
@@ -343,14 +343,12 @@ class NX_show_tasks (gdb.Command):
 NX_show_task()
 NX_show_tasks()
 
-class NX_show_heap (gdb.Command):
+class NuttXHeap ():
     """(NuttX) prints the heap"""
 
-    def __init__(self):
-        super(NX_show_heap, self).__init__('show heap', gdb.COMMAND_USER)
+    def __init__(self, heap_name):
         struct_mm_allocnode_s = gdb.lookup_type('struct mm_allocnode_s')
         preceding_size = struct_mm_allocnode_s['preceding'].type.sizeof
-        # preceding_size = 4
         if preceding_size == 2:
             self._allocflag = 0x8000
         elif preceding_size == 4:
@@ -358,6 +356,8 @@ class NX_show_heap (gdb.Command):
         else:
             raise gdb.GdbError('invalid mm_allocnode_s.preceding size %u' % preceding_size)
         self._allocnodesize = struct_mm_allocnode_s.sizeof
+        self._heap_name = heap_name
+        self._heap_information = []
 
     def _node_allocated(self, allocnode):
         if allocnode['preceding'] & self._allocflag:
@@ -367,7 +367,7 @@ class NX_show_heap (gdb.Command):
     def _node_size(self, allocnode):
         return allocnode['size'] & ~self._allocflag
 
-    def _print_allocations(self, region_start, region_end):
+    def _parse_allocations(self, region_start, region_end):
         if region_start >= region_end:
             raise gdb.GdbError('heap region {} corrupt'.format(hex(region_start)))
         nodecount = region_end - region_start
@@ -379,21 +379,44 @@ class NX_show_heap (gdb.Command):
                 state = ''
             else:
                 state = '(free)'
-            print( '  {} {} {}'.format(allocnode.address + self._allocnodesize,
-                                                  self._node_size(allocnode), state))
+            s = '  {} {} {}'.format(allocnode.address + self._allocnodesize,
+                                                  self._node_size(allocnode), state)
+            self._heap_information.append(s)
             cursor += self._node_size(allocnode) / self._allocnodesize
 
-    def invoke(self, args, from_tty):
-        heap = gdb.lookup_global_symbol('g_mmheap').value()
+    def heap_trace(self):
+        self._heap_information = []
+        heap = gdb.lookup_global_symbol(self._heap_name).value()
         nregions = heap['mm_nregions']
         region_starts = heap['mm_heapstart']
         region_ends = heap['mm_heapend']
         print( '{} heap(s)'.format(nregions))
         # walk the heaps
         for i in range(0, nregions):
-            self._print_allocations(region_starts[i], region_ends[i])
+            if region_starts[i] != 0:
+                self._parse_allocations(region_starts[i], region_ends[i])
+        return self._heap_information
+
+class NX_show_heap(gdb.Command):
+    """(NuttX) prints the heap"""
+
+    def __init__(self):
+        super(NX_show_heap, self).__init__("show heap", gdb.COMMAND_STACK)
+
+    def invoke(self, arg, from_tty):
+        if arg == 'kernel':
+            heap_variable_name = 'g_kmmheap'
+        else:
+            heap_variable_name = 'g_mmheap'
+        heap_information = NuttXHeap(heap_variable_name)
+        print('Showing heap allocations for %s (%s)' % (arg, heap_variable_name))
+        if heap_information is not None:
+            for line in heap_information.heap_trace():
+                print(line)
 
 NX_show_heap()
+
+
 
 # class NX_show_free_heap (gdb.Command):
 #     """(NuttX) prints the heap"""
