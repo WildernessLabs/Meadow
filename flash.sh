78f52b5b869efbae365ff650d5615a3dd0cb61b7
Mark Stevens <mark.stevens@wildernesslabs.co>
2022-10-11 08:37:11 +0100

    Flash script can now flash RT as well as OS.

--- flash.sh
+++ flash.sh
@@ -34,6 +34,8 @@ FORCE=false
 ESP=false
 CUBE=false
 OS_ONLY=false
+INCLUDE_RUNTIME=false
+RESET_BOARD=false
 
 for i in "$@"
 do
@@ -58,6 +60,12 @@ case $i in
     ;;
     -osonly|--osonly)
     OS_ONLY=true
+    ;;
+    -rt|--includeruntime)
+    INCLUDE_RUNTIME=true
+    ;;
+    -r|--reset)
+    RESET_BOARD=true
     ;;
     *)
     # unknown option
@@ -88,6 +96,19 @@ check_command_status() {
     exit 1
   else
     printf " ${green}success${reset}\n"
+  fi
+}
+
+#
+#   Reset the board if requested and possible.  This requires STM32CuberProgrammer
+#   to be installed and the CUBE_APP environment variable set to point to the CLI component.
+#   On Mac the CUBE_APP will be something like this:
+#
+#   CUBE_APP=/Applications/STMicroelectronics/STM32CubeProgrammer.app/Contents/MacOs/bin/STM32_Programmer_CLI 
+#
+reset_meadow() {
+  if [[ ! -z "${CUBE_APP}" ]] && [ "$RESET_BOARD" = true ] ; then
+    run_command "${CUBE_APP} -c port=swd -hardRst"
   fi
 }
 
@@ -127,58 +148,32 @@ if [ "$DFU" = true ] ; then
 fi
 
 #
-#   Use the STMCubeProgrammer and the CLI tool to flash the board.
-#     - Disable Mono
-#     - Erase flash memory
-#     - Flash the OS
-#     - Write the runtime system to the board
-#     - Copy the runtime into flash
-#
-if [ "$CUBE" = true ] ; then
-  printf "Flashing binaries using STMCubeProgrammer and Meadow.CLI.exe tool.\n"
-  if [ -z "${CUBE_APP}" ]; then
-    printf "The environment variable CUBE_APP must be set to point to the STMCubeProgrammer CLI application.\n"
-    printf "Typically this is something like /Applications/STMicroelectronics/STM32CubeProgrammer.app/Contents/MacOs/bin/STM32_Programmer_CLI\n"
-    exit -1
-  fi
-  if [ -z "${MEADOW_CLI_APP}" ]; then
-    printf "The environment variable MEADOW_CLI_APP should point to the Meadow.CLI.exe binary\n"
-    exit -1
-  fi
-  COMMAND="mono ${MEADOW_CLI_APP} --MonoDisable -s /dev/tty.usbmodem01"
-  printf "Executing: $COMMAND"
-  run_command "$COMMAND"
-  COMMAND="${CUBE_APP} -c port=swd --erase all"
-  printf "Executing: $COMMAND"
-  run_command "$COMMAND"
-  COMMAND="${CUBE_APP} -c port=swd --write $scriptdir/nuttx/Meadow.OS.bin 0x08000000 --verify"
-  printf "Executing: $COMMAND"
-  run_command "$COMMAND"
-  COMMAND="${CUBE_APP} -c port=swd -hardRst"
-  printf "Executing: $COMMAND"
-  run_command "$COMMAND"
-  sleep 1
-  printf "Executing: $COMMAND"
-  run_command "$COMMAND"
-  sleep 1
-  if [ "$OS_ONLY" != true ]; then
-    COMMAND="mono ${MEADOW_CLI_APP} --WriteFile -f $scriptdir/nuttx/Meadow.OS.Runtime.bin -s /dev/tty.usbmodem01"
-    printf "Executing: $COMMAND"
-    run_command "$COMMAND"
-    COMMAND="mono ${MEADOW_CLI_APP} --MonoFlash -s /dev/tty.usbmodem01"
-    printf "Executing: $COMMAND"
-    run_command "$COMMAND"
-  fi
-  exit 0
-fi
-
-#
 #   Flash the board with openocd
 #
-
 OCD=true
 printf "Flashing nuttx binaries using OpenOCD... "
 cd $scriptdir
+#
+#   First check if we should be writing the runtime system to the board.
+#
+if [ "$INCLUDE_RUNTIME" = true ] ; then
+  #
+  #   We reset the Meado board first in case it has recently been used in a debug session.
+  #   In the debug case it could be that GDB has left the board in a halted state.  This is
+  #   only required for the RT and flashing the OS with OCD will work even in the halted state.
+  #
+  reset_meadow
+  #
+  #   The sleep is necessary to allow the serial port to be represented by the Meadow board.
+  #
+  sleep 2
+  run_command "meadow mono disable"
+  run_command "meadow file write -f $scriptdir/nuttx/Meadow.OS.Runtime.bin"
+  run_command "meadow mono flash"
+fi
+#
+#   Next up try to flash the OS.
+#
 if [[ "$OS" == "mac" ]]; then
   #
   # Custom version of nuttx aware openocd
@@ -191,5 +186,7 @@ elif [[ "$OS" == "linux" ]]; then
   run_command "openocd -s //usr/local/share/openocd/scripts -f $scriptdir/flash.cfg"
 else
   printf "Unsupported OS ${bold}$OS${reset}.\n"
+  exit -1
 fi
+reset_meadow
 check_command_status
