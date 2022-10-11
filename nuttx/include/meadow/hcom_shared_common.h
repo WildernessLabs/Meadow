b7ac25d8761466295bf39f999ed74638282feeab
Mark Stevens <mark.stevens@wildernesslabs.co>
2022-10-11 11:30:31 +0100

    Turn trace messages on.

--- nuttx/include/meadow/hcom_shared_common.h
+++ nuttx/include/meadow/hcom_shared_common.h
@@ -399,7 +399,7 @@ typedef struct meadow_configuration_s meadow_configuration_t;
 // Adds code that takes the HCOM messages from CLI and outputs
 // a decoded version to syslog enable
 // HCOM_INCLUDE_DIAG_PRINT_BUFFER_CODE to add hex dump of HCOM messages
-#define HCOM_DIAG_INCLUDE_MESSAGE_DECODING_IN_BUILD   0
+#define HCOM_DIAG_INCLUDE_MESSAGE_DECODING_IN_BUILD   1
 
 // LOG_DEBUG syslog message are almost never used. Set this to 1
 // if you wish to have them compiled into Meadow
