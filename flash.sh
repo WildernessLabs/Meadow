b930b1000db64bae1be91e1e675396de6cf0e244
Mark Stevens <NevynUK@users.noreply.github.com>
2022-10-11 10:01:16 +0100

    Script improvements (#221)
    * Flash script can now flash RT as well as OS.
    
    * Removed unused options, added help and updated dfu command.
    
    * RT file is now deleted from the board after flash
    
    Co-authored-by: Mark Stevens <mark.stevens@wildernesslabs.co>

--- flash.sh
+++ flash.sh
@@ -32,12 +32,16 @@ fi
 VERBOSE=true
 FORCE=false
 ESP=false
-CUBE=false
-OS_ONLY=false
+INCLUDE_RUNTIME=false
+RESET_BOARD=false
+HELP=false
 
 for i in "$@"
 do
 case $i in
+    -h|--help)
+    HELP=true
+    ;;
     -v|--verbose)
     VERBOSE=true
     ;;
@@ -47,23 +51,35 @@ case $i in
     -dfu|--dfu)
     DFU=true
     ;;
-    -ocd|--ocd|--openocd)
-    OCD=true
-    ;;
     -esp|--esp)
     ESP=true
     ;;
-    -cube|--cube)
-    CUBE=true
+    -rt|--includeruntime)
+    INCLUDE_RUNTIME=true
     ;;
-    -osonly|--osonly)
-    OS_ONLY=true
+    -r|--reset)
+    RESET_BOARD=true
     ;;
     *)
-    # unknown option
+    echo "${0##*/} - Unknown option $i"
+    exit 1
     ;;
 esac
 done
+
+if [ "$HELP" = true ]; then
+  echo "Usage: ${0##*/} [options]"
+  echo " "
+  echo "Options:"
+  echo "  -h|--help                    Show this help message"
+  echo "  -v|--verbose                 Show verbose output"
+  echo "  -f|--force                   Force build"
+  echo "  -dfu|--dfu                   Use dfu-util to flash the board"
+  echo "  -esp|--esp                   Use idf.py script to flash the ESP code"
+  echo "  -rt|--includeruntime         Use the Meadow CLI tool to write the Mono runtime"
+  echo "  -r|--reset                   Use STM32CubeProgrammer to reset the board"
+  exit 0
+fi
 
 run_command() {
   if $VERBOSE; then
@@ -91,6 +107,19 @@ check_command_status() {
   fi
 }
 
+#
+#   Reset the board if requested and possible.  This requires STM32CuberProgrammer
+#   to be installed and the CUBE_APP environment variable set to point to the CLI component.
+#   On Mac the CUBE_APP will be something like this:
+#
+#   CUBE_APP=/Applications/STMicroelectronics/STM32CubeProgrammer.app/Contents/MacOs/bin/STM32_Programmer_CLI 
+#
+reset_meadow() {
+  if [[ ! -z "${CUBE_APP}" ]] && [ "$RESET_BOARD" = true ] ; then
+    run_command "${CUBE_APP} -c port=swd -hardRst"
+  fi
+}
+
 if [ "$ESP" = true ] ; then
   printf "Flashing ESP32 chip using ESP-PROG tool\n"
 
@@ -112,73 +141,41 @@ fi
 #
 #   Flash the board with dfu-util
 #
-
-DFU_COMMON_FLAGS='--alt 0 --path 20-1'
 if [ "$DFU" = true ] ; then
   printf "Flashing nuttx.bin using DFU... "
-  run_command "dfu-util $DFU_COMMON_FLAGS --download  $scriptdir/nuttx/nuttx.bin -s 0x08000000"
+  run_command "dfu-util -a 0 -D $scriptdir/nuttx/Meadow.OS.bin -s 0x08000000"
   check_command_status
-  if [ -f $scriptdir/nuttx/nuttx_user.bin ]; then
-    printf "Flashing nuttx_user.bin using DFU... "
-    run_command "dfu-util $DFU_COMMON_FLAGS --download $scriptdir/nuttx/nuttx_user.bin -s 0x08040000"
-    check_command_status
-  fi
-  exit 0
-fi
-
-#
-#   Use the STMCubeProgrammer and the CLI tool to flash the board.
-#     - Disable Mono
-#     - Erase flash memory
-#     - Flash the OS
-#     - Write the runtime system to the board
-#     - Copy the runtime into flash
-#
-if [ "$CUBE" = true ] ; then
-  printf "Flashing binaries using STMCubeProgrammer and Meadow.CLI.exe tool.\n"
-  if [ -z "${CUBE_APP}" ]; then
-    printf "The environment variable CUBE_APP must be set to point to the STMCubeProgrammer CLI application.\n"
-    printf "Typically this is something like /Applications/STMicroelectronics/STM32CubeProgrammer.app/Contents/MacOs/bin/STM32_Programmer_CLI\n"
-    exit -1
-  fi
-  if [ -z "${MEADOW_CLI_APP}" ]; then
-    printf "The environment variable MEADOW_CLI_APP should point to the Meadow.CLI.exe binary\n"
-    exit -1
-  fi
-  COMMAND="mono ${MEADOW_CLI_APP} --MonoDisable -s /dev/tty.usbmodem01"
-  printf "Executing: $COMMAND"
-  run_command "$COMMAND"
-  COMMAND="${CUBE_APP} -c port=swd --erase all"
-  printf "Executing: $COMMAND"
-  run_command "$COMMAND"
-  COMMAND="${CUBE_APP} -c port=swd --write $scriptdir/nuttx/Meadow.OS.bin 0x08000000 --verify"
-  printf "Executing: $COMMAND"
-  run_command "$COMMAND"
-  COMMAND="${CUBE_APP} -c port=swd -hardRst"
-  printf "Executing: $COMMAND"
-  run_command "$COMMAND"
-  sleep 1
-  printf "Executing: $COMMAND"
-  run_command "$COMMAND"
-  sleep 1
-  if [ "$OS_ONLY" != true ]; then
-    COMMAND="mono ${MEADOW_CLI_APP} --WriteFile -f $scriptdir/nuttx/Meadow.OS.Runtime.bin -s /dev/tty.usbmodem01"
-    printf "Executing: $COMMAND"
-    run_command "$COMMAND"
-    COMMAND="mono ${MEADOW_CLI_APP} --MonoFlash -s /dev/tty.usbmodem01"
-    printf "Executing: $COMMAND"
-    run_command "$COMMAND"
-  fi
   exit 0
 fi
 
 #
 #   Flash the board with openocd
 #
-
 OCD=true
 printf "Flashing nuttx binaries using OpenOCD... "
 cd $scriptdir
+#
+#   First check if we should be writing the runtime system to the board.
+#
+if [ "$INCLUDE_RUNTIME" = true ] ; then
+  #
+  #   We reset the Meado board first in case it has recently been used in a debug session.
+  #   In the debug case it could be that GDB has left the board in a halted state.  This is
+  #   only required for the RT and flashing the OS with OCD will work even in the halted state.
+  #
+  reset_meadow
+  #
+  #   The sleep is necessary to allow the serial port to be represented by the Meadow board.
+  #
+  sleep 2
+  run_command "meadow mono disable"
+  run_command "meadow file write -f $scriptdir/nuttx/Meadow.OS.Runtime.bin"
+  run_command "meadow mono flash"
+  run_command "meadow file delete -f Meadow.OS.Runtime.bin"
+fi
+#
+#   Next up try to flash the OS.
+#
 if [[ "$OS" == "mac" ]]; then
   #
   # Custom version of nuttx aware openocd
@@ -191,5 +188,7 @@ elif [[ "$OS" == "linux" ]]; then
   run_command "openocd -s //usr/local/share/openocd/scripts -f $scriptdir/flash.cfg"
 else
   printf "Unsupported OS ${bold}$OS${reset}.\n"
+  exit -1
 fi
+reset_meadow
 check_command_status
