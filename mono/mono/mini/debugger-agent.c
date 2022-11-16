4d9335cb2b4a775edfb2c3ea978d94f999b396a7
Alexis Christoforides <alexis@thenull.net>
2022-11-16 14:01:23 -0500

    [debugger] Disable metadata blob command.
    It is incompatible with the debugger libs bundled with latest Visual Studio

--- mono/mono/mini/debugger-agent.c
+++ mono/mono/mini/debugger-agent.c
@@ -7737,12 +7737,10 @@ assembly_commands (int command, guint8 *p, guint8 *end, Buffer *buf)
 		break;
 	}
     case CMD_ASSEMBLY_GET_METADATA_BLOB: {
-        MonoImage* image = ass->image;
-        if (ass->dynamic) {
-            return ERR_NOT_IMPLEMENTED;
-        }
-        buffer_add_byte_array (buf, (guint8*)image->raw_data, image->raw_data_len);
-        break;
+		/* we are incompatible with use of this command in the
+		 latest Mono.Debugging.Soft libs, and thus latest VS */
+
+		return ERR_NOT_IMPLEMENTED;
     }
     case CMD_ASSEMBLY_GET_IS_DYNAMIC: {
         buffer_add_byte (buf, ass->dynamic);
