78a7d95c71741cf021b2ffed374b0cd104f4c571
Peter-Moody <pwmoody@gmail.com>
2023-03-15 10:13:07 -0700

    The mono enable bit, in the meadow battery backed register, is set (which disables mono) if there is a version mismatch between Meadow.OS and mono.

--- apps/examples/hcom/mono/hcom_mono_control.c
+++ apps/examples/hcom/mono/hcom_mono_control.c
@@ -576,12 +576,15 @@ bool hcom_mono_ctrl_do_versions_matched()
 
       if (!osVersionMatch)
       {
+        // Disable mono if version mismatch
+        hcom_bbreg_set_bbr_bits(HCOM_BBREG_USER_RQST_MONO_ENABLE_BIT);
+
         // Meadow and mono versions don't match
         errReason = zalloc(HCOM_LARGE_HOST_STRING_BUFF_LENGTH);
         if (errReason != NULL)
         {
           snprintf_chk(errReason, HCOM_LARGE_HOST_STRING_BUFF_LENGTH,
-                      "Mono will not start - version mismatch: Meadow.OS version %s, Mono version %s",
+                      "Mono will not start - version mismatch: Meadow.OS version %s, Mono version %s, Mono disabled",
                       config->os_version.short_string, config->mono_version.short_string);
         }
         else
