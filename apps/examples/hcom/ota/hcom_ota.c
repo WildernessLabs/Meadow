0a7c724642362c3ca6b9f739609c27ee1326345c
Alexis Christoforides <alexis@thenull.net>
2023-03-20 15:37:00 -0300

    [OTA] Use CBC mode AES in our Meadow Cloud auth process (#346)

--- apps/examples/hcom/ota/hcom_ota.c
+++ apps/examples/hcom/ota/hcom_ota.c
@@ -173,11 +173,12 @@ int meadow_cloud_decrypt_buf(const char *encrypted_buf, int encrypted_len, const
 
 }
 
-int meadow_cloud_decrypt_buf_aes(const char *encrypted_buf, int encrypted_len, const char *key, int key_len, const char *decrypted_buf)
+int meadow_cloud_decrypt_buf_aes(const char *encrypted_buf, int encrypted_len, unsigned char key[16], unsigned char iv[16], const char *decrypted_buf)
 {
     mbedtls_aes_context aes;
     mbedtls_aes_init(&aes);
-    mbedtls_aes_setkey_dec(&aes, (const unsigned char*) key, key_len * 8 );
-    mbedtls_aes_crypt_ecb(&aes, MBEDTLS_AES_DECRYPT, (const unsigned char*)encrypted_buf, (const unsigned char*)decrypted_buf);
-    mbedtls_aes_free( &aes );    
+    mbedtls_aes_setkey_dec(&aes, (const unsigned char*) key, 256 );
+    mbedtls_aes_crypt_cbc(&aes, MBEDTLS_AES_DECRYPT, encrypted_len, iv, (const unsigned char*)encrypted_buf, (const unsigned char*)decrypted_buf);
+    mbedtls_aes_free( &aes );
+    return encrypted_len;
 }
--- apps/examples/mono/mappings-meadow.h
+++ apps/examples/mono/mappings-meadow.h
@@ -15,7 +15,7 @@ extern int tcsetattr(int fd, int options, FAR const struct termios *termiosp);
 extern int mount(const char *source, const char *target, const char *filesystemtype, unsigned long mountflags, const void *data);
 extern int umount2(const char *target, int flags);
 extern int meadow_cloud_decrypt_buf(const char *encrypted_buf, int encrypted_len, const char *decrypted_buf);
-extern int meadow_cloud_decrypt_buf_aes(const char *encrypted_buf, int encrypted_len, const char *key, int key_len, const char *decrypted_buf);
+extern int meadow_cloud_decrypt_buf_aes(const char *encrypted_buf, int encrypted_len, unsigned char key[16], unsigned char iv[16], const char *decrypted_buf);
 
 int shim_open_void(char *pathname, int flags);
 
