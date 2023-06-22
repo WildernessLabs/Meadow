3d2ae3539b69c988fc6f874bbdc088c874ddc096
Eduardo Menezes <eduardomenezesm@msn.com>
2023-06-22 19:04:40 -0300

    Adjust PPPD thread priority

--- apps/examples/hcom/misc/hcom_pppd.c
+++ apps/examples/hcom/misc/hcom_pppd.c
@@ -268,7 +268,21 @@ int hcom_pppd_start()
             return EINVAL;
         }
 
-        ret = pthread_create(&pppd_thread_id, NULL, pppd_thread, (void *) &cell_settings);
+        pthread_attr_t attr;
+        struct sched_param param;
+
+        // Initialize thread attributes
+        pthread_attr_init(&attr);
+
+        // Set the scheduling policy to SCHED_FIFO (First-In, First-Out)
+        pthread_attr_setinheritsched(&attr, PTHREAD_EXPLICIT_SCHED);
+        pthread_attr_setschedpolicy(&attr, SCHED_FIFO);
+
+        // Set the priority of the thread
+        param.sched_priority = 200;
+        pthread_attr_setschedparam(&attr, &param);
+
+        ret = pthread_create(&pppd_thread_id, &attr, pppd_thread, (void *) &cell_settings);
         if (ret == OK)
         {
             hcom_logging_syslog(LOG_INFO, "%s@%d-PPPD thread launched\n", thisFile, __LINE__);
