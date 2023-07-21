40195752d43a18ed66036f2cf532790293fc78a8
halyssonJr <halysson1007@gmail.com>
2023-07-20 21:19:26 -0300

    #416 add on mapping scanner networking function

--- apps/examples/mono/mappings-meadow.h
+++ apps/examples/mono/mappings-meadow.h
@@ -16,6 +16,7 @@ extern int mount(const char *source, const char *target, const char *filesystemt
 extern int umount2(const char *target, int flags);
 extern int meadow_cloud_decrypt_buf(const char *encrypted_buf, int encrypted_len, const char *decrypted_buf);
 extern int meadow_cloud_decrypt_buf_aes(const char *encrypted_buf, int encrypted_len, unsigned char key[16], unsigned char iv[16], const char *decrypted_buf);
+extern int hcom_pppd_scanner(FAR char *response);
 
 int shim_open_void(char *pathname, int flags);
 
@@ -64,6 +65,7 @@ MonoDlMapping meadow_mappings[] = {
        { "mallinfo", mallinfo },
        { "meadow_cloud_decrypt_buf", meadow_cloud_decrypt_buf },
        { "meadow_cloud_decrypt_buf_aes", meadow_cloud_decrypt_buf_aes },
+       { "hcom_pppd_scanner", hcom_pppd_scanner}
        
 //       { "poll", poll },
 
