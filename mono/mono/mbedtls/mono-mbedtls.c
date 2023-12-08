09b1931c575391711c76d48295355770f8af59eb
halyssonJr <halysson1007@gmail.com>
2023-12-08 15:18:41 -0300

    add notification to server and separete handshake process.

--- mono/mono/mbedtls/mono-mbedtls.c
+++ mono/mono/mbedtls/mono-mbedtls.c
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <stdbool.h>
 #include "eglib/glib.h"
 #include "metadata/fdhandle.h"
 #include "mbedtls/net_sockets.h"
@@ -3289,9 +3290,19 @@ const unsigned char root_ca_pems[] = "-----BEGIN CERTIFICATE-----\n"
 "-----END CERTIFICATE-----";
 
 int root_ca_pems_len = sizeof(root_ca_pems);
+int is_clear = 0;
 
-#define DEV_URANDOM_THRESHOLD        32
+#define DEV_URANDOM_THRESHOLD       32
 #define DEV_RANDOM_THRESHOLD        32
+#define FAILED                      -1
+
+#define NO_DEBUG            0
+#define DEBUG_ERROR         1
+#define DEBUG_STATE_CHANGE  2
+#define DEBUG_INFO          3
+#define DEBUG_VERBOSE       4
+
+#define DEBUG_THRESHOLD     NO_DEBUG
 
 // copied from mbedtls/programs/pkey/gen_key.c
 static int dev_random_entropy_poll( void *data, unsigned char *output,
@@ -3328,13 +3339,13 @@ static int dev_random_entropy_poll( void *data, unsigned char *output,
     return( 0 );
 }
 
-int mono_mbedtls_init ()
+int mono_mbedtls_init (void)
 {
     mono_mbedtls_initialized = TRUE;
 
     int ret;
     mbedtls_ssl_config_init( &conf );
-    mbedtls_debug_set_threshold(0);
+    mbedtls_debug_set_threshold(DEBUG_THRESHOLD);
     
     FILE *client_cert_file = fopen( client_cert_path, "r" );
     if ( client_cert_file )
@@ -3428,43 +3439,69 @@ intptr_t mono_mbedtls_connect (intptr_t mono_fd, intptr_t readbuf, intptr_t writ
     mbedtls_ssl_context *ssl = NULL;
 
     SocketHandle *sockethandle;
-    if (!mono_fdhandle_lookup_and_ref (mono_fd, (MonoFDHandle**) &sockethandle)) {
+    if (!mono_fdhandle_lookup_and_ref (mono_fd, (MonoFDHandle**) &sockethandle)) 
+    {
         printf ("Socket FD not found!\n");
         return NULL;
     }
 
     server_fd = g_malloc (sizeof(mbedtls_net_context));
+    if (server_fd == NULL)
+    {
+        printf ("Failed to allocate socket descriptor\n");
+        goto error;
+    }
+
+    // Wrapper of the socket descriptor
     mbedtls_net_init( server_fd );
     server_fd->fd = sockethandle->fdhandle.fd;
 
     ssl = g_malloc (sizeof(mbedtls_ssl_context));
+    if (ssl == NULL)
+    {
+        printf ("Failed to allocate ssl context\n");
+        goto error;
+    }
+
+    // TLS conext which represent the session
     mbedtls_ssl_init( ssl );
 
     int ret;
 
-    //SSL Connection
-    ret = mbedtls_ssl_setup (ssl, &conf);
-    if( ( ret = mbedtls_ssl_set_hostname( ssl, hostname ) ) != 0 ) {
-        printf( " failed\n ! mbedtls_ssl_set_hostname returned %d\n\n", ret );
+    //Assing the TLS config to the TLS context 
+    if (ret = mbedtls_ssl_setup (ssl, &conf) != 0)
+    {
+        printf( "mbedtls_ssl_setup returned -0x%x\n", -ret );
         goto error;
     }
 
-    if ( clicert != NULL && pkey != NULL ) {
+    // Set hostname for verification
+    if (( ret = mbedtls_ssl_set_hostname( ssl, hostname ) ) != 0 ) 
+    {
+        printf( "mbedtls_ssl_set_hostname returned -0x%x\n", -ret );
+        goto error;
+    }
+
+    if ( clicert != NULL && pkey != NULL ) 
+    {
         // Configure SSL context with client certificate and private key
-        if ( ( ret = mbedtls_ssl_conf_own_cert( &conf, clicert, pkey ) ) != 0 ) {
+        if ( ( ret = mbedtls_ssl_conf_own_cert( &conf, clicert, pkey ) ) != 0 ) 
+        {
             printf( " failed to configure client certificate and private key %d\n\n", ret );
             goto error;
         } 
     }
 
+    // Link the socket wrapper to the TLS session structure
     mbedtls_ssl_set_bio( ssl, server_fd, mbedtls_net_send, mbedtls_net_recv, NULL );
 
-    ret = mbedtls_ssl_handshake (ssl);
-
-    if (ret < 0)
-        goto error;
-
     MonoMbedTlsContext *new_ctx = g_malloc (sizeof(MonoMbedTlsContext));
+    if (new_ctx == NULL)
+    {
+        printf("MonoMbedTlsConext failed to be allocated\n");
+        goto error;
+    }
+
     new_ctx->read_buf = readbuf;
     new_ctx->write_buf = writebuf;
     new_ctx->mbedtls_ctx = ssl;
@@ -3472,6 +3509,8 @@ intptr_t mono_mbedtls_connect (intptr_t mono_fd, intptr_t readbuf, intptr_t writ
     return new_ctx;
 
 error:
+    printf(" Failed to Connected \n");
+
     if (ssl) {
         mbedtls_ssl_free (ssl);
         g_free (ssl);
@@ -3483,9 +3522,32 @@ error:
     return NULL;
 }
 
+int mono_mbedtls_handshake(MonoMbedTlsContext *ctx)
+{
+    int ret = FAILED;
+
+    if (ctx->mbedtls_ctx != NULL)
+    {
+        // Perform the TLS handshake
+        ret = mbedtls_ssl_handshake(ctx->mbedtls_ctx);
+        if (ret != 0)
+        {
+            printf("Handshake failed\n");
+        }
+    }
+    return ret;
+}
+
 int mono_mbedtls_read (MonoMbedTlsContext * ctx, int length)
 {
-    int ret = mbedtls_ssl_read(ctx->mbedtls_ctx, ctx->read_buf, length);
+    int ret = FAILED;
+
+    if (ctx->mbedtls_ctx != NULL && ctx->read_buf != NULL)
+    {
+        char *buffer = (char *)ctx->read_buf;
+        memset(buffer, 0, sizeof(buffer));
+        ret = mbedtls_ssl_read(ctx->mbedtls_ctx, buffer, length);
+    }
     return ret;
 }
 
@@ -3513,11 +3575,29 @@ exit:
 
 void mono_mbedtls_close (MonoMbedTlsContext * ctx)
 {
-    mbedtls_ssl_free (ctx->mbedtls_ctx);
-    g_free (ctx->mbedtls_ctx);
-    mbedtls_net_free (ctx->mbedtls_fd);
-    g_free (ctx->mbedtls_fd);
-    g_free (ctx);
+    int ret = FAILED;
+
+    if (ctx != NULL)
+    {
+        /* Close the connection by sending "close notify" message to the server. */
+        if ((ret = mbedtls_ssl_close_notify(ctx->mbedtls_ctx) != 0))
+        {
+            printf("mbedtls_ssl_close_notify returned: -0x%x\n", -ret);
+        }
+
+        if (ctx->mbedtls_ctx)
+        {
+            mbedtls_ssl_free (ctx->mbedtls_ctx);
+            g_free (ctx->mbedtls_ctx);
+        }
+
+        if (ctx->mbedtls_fd)
+        {
+            mbedtls_net_free (ctx->mbedtls_fd);
+            g_free (ctx->mbedtls_fd);
+        }
+        g_free (ctx);
+    }
     return;
 }
 
