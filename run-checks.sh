7037a466b10ac8b5642a2897c77e22c8303adac7
Mark Stevens <mark@thepcsite.co.uk>
2024-01-03 13:04:40 +0000

    Fixed duplicate method definition.

--- run-checks.sh
+++ run-checks.sh
@@ -40,31 +40,5 @@ if [ "$HELP" = true ]; then
   exit 0
 fi
 
-run_command() {
-  if $VERBOSE || $DRYRUN; then
-    echo "Executing command: $1"
-    if ! $DRYRUN; then
-        $1
-    fi
-  else
-    if ! $DRYRUN; then
-        $1 &>/dev/null
-    fi
-  fi
-}
-
-check_command_status() {
-  exit_status=$?
-  if [ $exit_status -ne 0 ]; then
-    printf " ${red}error${reset}\n"
-    if ! $VERBOSE; then
-        printf "Re-run the script with --verbose flag to see the output.\n"
-    fi
-    exit 1
-  else
-    printf " ${green}success${reset}\n"
-  fi
-}
-
 cppcheck --error-exitcode=1 --quiet --check-level=exhaustive --force --inline-suppr $CPPCHECK_NUTTX_IGNORE_LIST $scriptdir/nuttx/configs/stm32f777zit6-meadow/src
 cppcheck --error-exitcode=1 --quiet --check-level=exhaustive --force --inline-suppr $scriptdir/apps/examples/hcom
