521437c02d0cc10978c9dfb3b70bcd76b3ed561e
Alexis Christoforides <alexis@thenull.net>
2024-01-12 11:27:43 -0500

    [mono] Enable perfcounters (#544)

--- build-mono.sh
+++ build-mono.sh
@@ -109,7 +109,7 @@ CONFIGURE="./configure
     --enable-mbedtls=yes
     --enable-nuttx=yes
     --enable-minimal=profiler,pinvoke,debug,appdomains,verifier,large_code,logging,\
-com,attach,simd,perfcounters,normalization,desktop_loader,shared_perfcounters,\
+com,attach,simd,normalization,desktop_loader,shared_perfcounters,\
 remoting,security,lldb,mdb,shadowcopy"
 
 if $NETCORE; then
