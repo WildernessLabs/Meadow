58397ff79dccb88e7c28eec73c9c3330082e7a9d
Alexis Christoforides <alexis@thenull.net>
2024-01-15 14:02:34 -0500

    Bump version for 1.8 release

--- mono/mono/metadata/threadpool-worker-default.c
+++ mono/mono/metadata/threadpool-worker-default.c
@@ -36,12 +36,6 @@
 #include <mono/utils/w32api.h>
 #include <mono/utils/mono-complex.h> // This header has defines to muck with names, so put it late.
 
-#if defined(__NuttX__)
-// This is ghastly, but cabs and creal are in libm.  We need our own libm.
-
-int __errno;
-#endif
-
 #define CPU_USAGE_LOW 80
 #define CPU_USAGE_HIGH 95
 
