531b0aeac1b306190d1a7407393caca4ae83d340
Eduardo Menezes <eduardomenezesm@msn.com>
2024-02-23 08:32:11 -0300

    Add __NuttX__ defines

--- mono/mono/metadata/profiler.c
+++ mono/mono/metadata/profiler.c
@@ -15,7 +15,9 @@
 #include <mono/utils/mono-dl.h>
 #include <mono/utils/mono-error-internals.h>
 #include <mono/utils/mono-logger-internals.h>
+#if defined(__NuttX__)
 #include <mono/profiler/log.h>
+#endif
 
 MonoProfilerState mono_profiler_state;
 
@@ -141,27 +143,50 @@ load_profiler_from_installation (const char *libname, const char *name, const ch
  * This function may \b only be called by embedders prior to running managed
  * code.
  */
+void
+mono_profiler_load (const char *desc)
+{
+	const char *col;
+	char *mname, *libname;
 
-void mono_profiler_load(const char *desc) {
-    const char *col;
-    char *mname;
+	mname = libname = NULL;
 
-    mname = NULL;
+	if (!desc || !strcmp ("default", desc))
+		desc = "log:report";
 
-    if (!desc || !strcmp("default", desc))
-        desc = "log:report";
-
-    if ((col = strchr(desc, ':')) != NULL) {
-        mname = (char *)g_memdup(desc, col - desc + 1);
-        mname[col - desc] = 0;
-    } else {
-        mname = g_strdup(desc);
-    }
-
-    // Call the profiler initialization function directly
+	if ((col = strchr (desc, ':')) != NULL) {
+		mname = (char *) g_memdup (desc, col - desc + 1);
+		mname [col - desc] = 0;
+	} else {
+		mname = g_strdup (desc);
+	}
+#if defined(__NuttX__)
+	if (!desc || strcmp ("log", desc))
+		mono_trace (G_LOG_LEVEL_DEBUG, MONO_TRACE_PROFILER, "Profiler module %s not supported!", desc);
+   
+	// Call the profiler initialization function using static libs
     mono_profiler_init_log(desc);
+	goto done;
+#else
+	if (load_profiler_from_executable (mname, desc))
+		goto done;
 
-    g_free(mname);
+	libname = g_strdup_printf ("mono-profiler-%s", mname);
+
+	if (load_profiler_from_installation (libname, mname, desc))
+		goto done;
+
+	if (mono_config_get_assemblies_dir () && load_profiler_from_directory (mono_assembly_getrootdir (), libname, mname, desc))
+		goto done;
+
+	if (load_profiler_from_directory (NULL, libname, mname, desc))
+		goto done;
+
+	mono_trace (G_LOG_LEVEL_CRITICAL, MONO_TRACE_PROFILER, "The '%s' profiler wasn't found in the main executable nor could it be loaded from '%s'.", mname, libname);
+#endif
+done:
+	g_free (mname);
+	g_free (libname);
 }
 
 /**
