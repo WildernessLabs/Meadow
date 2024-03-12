523d05be44569c569be8fbbd5db58cf02c1da92e
Eduardo Menezes <eduardomenezesm@msn.com>
2024-03-11 21:25:01 -0300

    Minor fixes on uart mapping

--- nuttx/configs/stm32f777zit6-meadow/src/hcom_nx/hcom_nx_config_manager.c
+++ nuttx/configs/stm32f777zit6-meadow/src/hcom_nx/hcom_nx_config_manager.c
@@ -180,6 +180,7 @@ const struct meadow_uart_mapping_s hcom_nx_uart_mapping [] =
 {
     { MEADOW_UART1_NAME, MEADOW_COM1_NAME},
     { MEADOW_UART4_NAME, MEADOW_COM4_NAME},
+    { MEADOW_UART6_NAME, MEADOW_COM6_NAME},
 }; 
 
 /****************************************************************************
@@ -380,17 +381,9 @@ void hcom_nx_config_map_cell_network_mode(meadow_configuration_t *config)
  ****************************************************************************/
 static void hcom_nx_config_map_cell_com_port(meadow_configuration_t *config, yaml_cell_config_t *settings)
 {
-    int i;
-
-    if (settings == NULL)
+    if (settings == NULL || config == NULL)
     {
-        syslog(LOG_ERR, "Failed getting settings\n");
-        return;
-    }
-
-    if (config == NULL)
-    {
-        syslog(LOG_ERR, "Failed getting configuration\n");
+        syslog(LOG_ERR, "Failed getting default cell settings");
         return;
     }
 
@@ -398,16 +391,14 @@ static void hcom_nx_config_map_cell_com_port(meadow_configuration_t *config, yam
     {
         if (config->use_uart1_for_trace != 0)
         {
-            syslog(LOG_INFO, "COM1 is already use for trace!!!\n");
-            config->default_cell_settings->ttyname = kmm_strdup(MEADOW_UART4_NAME);
-            return;
+            syslog(LOG_WARNING, "COM1 is already in use for tracing!\n");
         }
 
-        for (i = 0; i < sizeof(hcom_nx_uart_mapping)/sizeof(hcom_nx_uart_mapping[0]); i++)
+        for (int i = 0; i < sizeof(hcom_nx_uart_mapping) / sizeof(hcom_nx_uart_mapping[0]); i++)
         {
             if (strcmp(settings->settings->ttyname, hcom_nx_uart_mapping[i].com_name) == 0)
             {
-                config->default_cell_settings->ttyname = kmm_strdup (hcom_nx_uart_mapping[i].tty_name);
+                config->default_cell_settings->ttyname = kmm_strdup(hcom_nx_uart_mapping[i].tty_name);
                 return;
             }
         }
