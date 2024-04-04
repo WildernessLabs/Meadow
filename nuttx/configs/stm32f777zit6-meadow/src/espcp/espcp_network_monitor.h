98f6277ed38e3c5316982fe5b5c4f480500ee094
Mark Stevens <mark@thepcsite.co.uk>
2024-04-04 10:01:45 +0100

    Corrected file name

--- nuttx/configs/stm32f777zit6-meadow/src/espcp/espcp_network_monitor.h
+++ nuttx/configs/stm32f777zit6-meadow/src/espcp/espcp_network_monitor.h
@@ -1,5 +1,5 @@
 /****************************************************************************
- * espcp_uart_comms.h
+ * espcp_network_monitor.h
  *
  *   Copyright (C) 2024 Wilderness Labs. All rights reserved.
  *   Author: Mark Stevens
