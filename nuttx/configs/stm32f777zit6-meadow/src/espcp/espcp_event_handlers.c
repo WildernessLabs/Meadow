b42cc9c108f855b93c5972694004c2fb2cde0dba
Mark Stevens <mark@thepcsite.co.uk>
2024-06-13 13:55:48 +0100

    Reduced the number of diagnostic messages.

--- nuttx/configs/stm32f777zit6-meadow/src/espcp/espcp_event_handlers.c
+++ nuttx/configs/stm32f777zit6-meadow/src/espcp/espcp_event_handlers.c
@@ -365,8 +365,6 @@ espcp_message_t *espcp_get_event_data(uint32_t message_id)
  ****************************************************************************/
 void espcp_dispatch_event(espcp_message_t *message)
 {
-    MEADOW_TRACE_INFORMATION("%s: Enter\n", __func__);
-
     if (message != NULL)
     {
         espcp_event_handlers_t *handler = NULL;
@@ -415,8 +413,6 @@ void espcp_dispatch_event(espcp_message_t *message)
             }
         }
     }
-
-    MEADOW_TRACE_INFORMATION("%s: Exit\n", __func__);
 }
 
 /****************************************************************************
@@ -667,6 +663,5 @@ void espcp_pass_to_managed_event_handler(espcp_message_t *message)
     {
         espcp_delete_message_and_payload(message);
     }
-
     MEADOW_TRACE_INFORMATION("%s: Exit\n", __func__);
 }
