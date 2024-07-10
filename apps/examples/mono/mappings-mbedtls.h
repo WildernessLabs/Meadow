96018e0f323fae6983c8cdef0949fb123c029a0d
Mark Stevens <mark@thepcsite.co.uk>
2024-07-10 19:30:19 +0100

    Added void to remove a warning

--- apps/examples/mono/mappings-mbedtls.h
+++ apps/examples/mono/mappings-mbedtls.h
@@ -2,7 +2,7 @@ extern intptr_t mono_mbedtls_connect (intptr_t mono_fd, intptr_t readbuf, intptr
 extern int mono_mbedtls_read (intptr_t * ctx, int length);
 extern int mono_mbedtls_write (intptr_t * ctx, int length);
 extern void mono_mbedtls_close (intptr_t * ctx);
-extern int mono_mbedtls_init ();
+extern int mono_mbedtls_init (void);
 extern int mono_mbedtls_set_server_cert_authmode (int authmode);
 extern int mono_mbedtls_handshake (intptr_t * ctx);
 
