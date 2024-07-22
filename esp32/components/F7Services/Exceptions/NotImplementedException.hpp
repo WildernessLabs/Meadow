7f0ad0adeb9a40799aa699ef42ad2a2aa197f32c
Mark Stevens <NevynUK@users.noreply.github.com>
2024-07-22 20:00:46 +0100

    Remove old copy of the ESP32 code.

--- esp32/components/F7Services/Exceptions/NotImplementedException.hpp
+++
@@ -1,9 +0,0 @@
-#include <stdexcept>
-
-
-class NotImplementedException : public std::runtime_error
-{
-public:
-    NotImplementedException() : std::runtime_error("NotImplementedException") { }
-};
-
