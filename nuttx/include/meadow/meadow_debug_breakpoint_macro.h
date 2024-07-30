d9d0a14b5d90b3767f3c83dd5ebae3fe74fa2205
Mark Stevens <NevynUK@users.noreply.github.com>
2024-07-30 20:05:35 +0100

    Add debug breakpoint macros (#657)
    * Add debug breakpoint macros
    
    * Fixed date in copyright message.

---
+++ nuttx/include/meadow/meadow_debug_breakpoint_macro.h
@@ -0,0 +1,57 @@
+/****************************************************************************
+ * /include/meadow/meadow_debug_breakpoint_macro.h
+ * 
+ *   Copyright (C) 2024 Wilderness Labs. All rights reserved.
+ *   Author:  Wilderness Labs
+ * 
+ *   Provide macros and method defintions to assist in debugging
+ *   NuttX and meadow code.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name NuttX nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+/**
+ * The contents of this file are really a convenience for debugging.  The macros
+ * held here will insert debug breakpoints into the code.  They are only meant to
+ * be used in debug scenarios and should not be left in production code.
+ */
+
+/**
+ * @brief Always insert a debug breakpoint into the code.
+ */
+#define MEADOW_DEBUG_BREAKPOINT() __asm__ ("bkpt");
+
+/**
+ * @brief Insert a debug breakpoint in the code that will trigger only if the
+ * condition is met.
+ */
+#define MEADOW_DEBUG_BREAKPOINT_IF(condition) \
+    if (condition) { \
+        MEADOW_DEBUG_BREAKPOINT(); \
+    }
