0a06c29a45f49ff58dd067fec5fa493a0dee1737
Eduardo Menezes <39597657+duduita@users.noreply.github.com>
2024-08-22 16:42:03 -0300

    Change MONO large object store allocation (#666)
    Change the Mono GC's LOS (Large Object Space) behavior to allocate memory in using malloc/free.

--- mono/mono/sgen/sgen-los.c
+++ mono/mono/sgen/sgen-los.c
@@ -130,6 +130,10 @@ static LOSFreeChunks *los_fast_free_lists [LOS_NUM_FAST_SIZES]; /* 0 is for larg
 static mword los_num_objects = 0;
 static int los_num_sections = 0;
 
+#if defined(__NuttX__)
+#define USE_MALLOC
+#endif
+
 //#define USE_MALLOC
 //#define LOS_CONSISTENCY_CHECK
 //#define LOS_DUMMY
