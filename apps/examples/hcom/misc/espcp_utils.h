a9acc0d99feb8768141fc37dab3a20f2da210459
Mark Stevens <mark@thepcsite.co.uk>
2025-05-18 09:12:48 +0100

    Remove declaration warnings

--- apps/examples/hcom/misc/espcp_utils.h
+++ apps/examples/hcom/misc/espcp_utils.h
@@ -96,6 +96,7 @@ typedef struct espcp_event_data_s espcp_event_data_t;
  ****************************************************************************/
 
 int espcp_queue_event_messages(uint8_t *message);
-int espcp_open_esp32_events_message_queue();
+int espcp_open_esp32_events_message_queue(void);
+void espcp_encode_event_data(espcp_event_data_t *, uint8_t *);  
 
 #endif //__CONFIGS_MEADOW_SRC_HCOM_MISC_ESPCP_UTILS__H
