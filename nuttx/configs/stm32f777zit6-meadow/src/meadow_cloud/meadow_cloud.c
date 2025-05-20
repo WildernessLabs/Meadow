fb2d83ec2fc3aee88972240318d4aae37921b28d
Mark Stevens <mark@thepcsite.co.uk>
2025-05-20 09:52:10 +0100

    Change const to make the pointer const and not the data pointed to.

--- nuttx/configs/stm32f777zit6-meadow/src/meadow_cloud/meadow_cloud.c
+++ nuttx/configs/stm32f777zit6-meadow/src/meadow_cloud/meadow_cloud.c
@@ -17,10 +17,10 @@ static int private_key_buf_size = 0;
 
 int meadow_cloud_provision(FAR const char *private_key_buf, int private_key_len, FAR const char *public_key_buf, int public_key_len, FAR void *unused)
 {
-    if (espcp_file_system_write_file(MEADOW_CLOUD_PRIVATE_KEY_FILE, private_key_buf, private_key_len) < 0)
+    if (espcp_file_system_write_file(MEADOW_CLOUD_PRIVATE_KEY_FILE, (uint8_t *) private_key_buf, private_key_len) < 0)
         return -1;
 
-    if (espcp_file_system_write_file(MEADOW_CLOUD_PUBLIC_KEY_FILE, public_key_buf, public_key_len) < 0)
+    if (espcp_file_system_write_file(MEADOW_CLOUD_PUBLIC_KEY_FILE, (uint8_t *) public_key_buf, public_key_len) < 0)
         return -2;
     return 0;
 }
@@ -28,7 +28,7 @@ int meadow_cloud_provision(FAR const char *private_key_buf, int private_key_len,
 int meadow_cloud_retrieve_private_key(FAR const char **private_key_buf_ptr, int *len)
 {
     int16_t length;
-    const char *buf = espcp_file_system_read_file(MEADOW_CLOUD_PRIVATE_KEY_FILE, &length);
+    const char *buf = (const char *) espcp_file_system_read_file(MEADOW_CLOUD_PRIVATE_KEY_FILE, &length);
     *private_key_buf_ptr = buf;
     if (buf == NULL)
         return -1;
@@ -38,7 +38,7 @@ int meadow_cloud_retrieve_private_key(FAR const char **private_key_buf_ptr, int 
     return 0;
 }
 
-int meadow_cloud_release_private_key(FAR const char **private_key_buf_ptr)
+int meadow_cloud_release_private_key(char ** const private_key_buf_ptr)
 {
     if (private_key_buf_size == 0)
         up_assert(__FILE__, __LINE__); // release without retrieve
--- nuttx/include/meadow/meadow_cloud.h
+++ nuttx/include/meadow/meadow_cloud.h
@@ -6,6 +6,6 @@
 
 int meadow_cloud_provision(FAR const char *, int, FAR const char *, int, FAR void*);
 int meadow_cloud_retrieve_private_key(FAR const char **private_key_buf_ptr, int *len);
-int meadow_cloud_release_private_key(FAR const char **private_key_buf_ptr);
+int meadow_cloud_release_private_key(char ** const private_key_buf_ptr);
 
 #endif
