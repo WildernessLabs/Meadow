71dc71a4c5e8d322cd37784f63f37eb6bdb6acaf
Mark Stevens <mark@thepcsite.co.uk>
2026-05-18 19:50:31 +0100

    Change tools repository.

--- checkout-submodules.sh
+++ checkout-submodules.sh
@@ -72,7 +72,9 @@ git submodule init
 git submodule update
 git submodule
 
-checkout_submodule "https://bitbucket.org/nuttx/tools.git" "tools"
+# checkout_submodule "https://bitbucket.org/nuttx/tools.git" "tools"
+checkout_submodule "https://gitlab.com/nuttx/tools.git" "tools"
+
 checkout_submodule_github "WildernessLabs/mbedtls" "mbedtls"
 
 clean_submodule .
